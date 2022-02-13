bb34066814548738b37c582121f9d825784a3703
Daniel Reina Ramos <drrmovil@gmail.com>
2022-02-13 10:07:14 +0100

    añadiendo callback para dar el mismo resultado

--- app.js
+++ app.js
@@ -2,14 +2,29 @@
 const hexValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
-btn.addEventListener('click', function () {
-    let hexColor = '#';
+//Esta es la solución del callback
+btn.addEventListener("click", function () {
+    generateColor((x) => {
+        color.textContent = x;
+        document.body.style.backgroundColor = x;
+    });
+});
+function generateColor(update) {
+    let hexColor = "#";
     for (let i = 0; i < 6; i++) {
         hexColor += hexValues[getRandomNumber()];
     }
-    color.textContent = hexColor;
-    document.body.style.backgroundColor = hexColor;
-});
+    update(hexColor);
+}
+//Esta es la solución inicial, luego se hizo otra solución que da el mismo resultado pero usando un callback.
+// btn.addEventListener('click', function(){
+//    let hexColor:string = '#'
+//    for(let i=0; i<6; i++){
+//        hexColor += hexValues[getRandomNumber()]
+//    }
+//    color.textContent = hexColor;
+//    document.body.style.backgroundColor = hexColor;
+// })
 function getRandomNumber() {
     return Math.floor(Math.random() * hexValues.length);
 }
--- app.ts
+++ app.ts
@@ -3,15 +3,34 @@ const hexValues:string[]=['0','1','2','3','4','5','6','7','8','9','A','B','C','D
 
 const btn = document.getElementById('btn')!;
 const color = document.querySelector('.color')!;
+//Esta es la solución del callback
+btn.addEventListener("click", function () {
+    generateColor((x) => {
+      color.textContent = x;
+      document.body.style.backgroundColor = x;
+  })
+  
+});
 
-btn.addEventListener('click', function(){
-   let hexColor:string = '#'
-   for(let i=0; i<6; i++){
-       hexColor += hexValues[getRandomNumber()]
-   }
-   color.textContent = hexColor;
-   document.body.style.backgroundColor = hexColor;
-})
+function generateColor(update: (color: string) => void) {
+    
+let hexColor: string = "#";
+for (let i = 0; i < 6; i++) {
+  hexColor += hexValues[getRandomNumber()];
+}
+update (hexColor)
+}
+
+//Esta es la solución inicial, luego se hizo otra solución que da el mismo resultado pero usando un callback.
+// btn.addEventListener('click', function(){
+//    let hexColor:string = '#'
+//    for(let i=0; i<6; i++){
+//        hexColor += hexValues[getRandomNumber()]
+//    }
+//    color.textContent = hexColor;
+//    document.body.style.backgroundColor = hexColor;
+// })
+
 function getRandomNumber(){
     return Math.floor(Math.random()*hexValues.length)
 }
